b874ef05f927ccd8436eb9f5fa66efcfc9c67b83
pooyaht <pooyahatami83@gmail.com>
2024-10-08 08:24:32 +0330

    Refactor scanner

--- internal/scanner/scanner.go
+++ internal/scanner/scanner.go
@@ -26,71 +26,71 @@ func (s *Scanner) Scan() (*token.Token, error) {
 		return &token.Token{Type: token.EOF, Lexeme: "EOF", Literal: nil}, nil
 	}
 
-	switch s.input[s.index] {
+	switch s.peak() {
 	case '(':
-		s.index++
+		s.advance()
 		return &token.Token{Type: token.LEFT_PAREN, Lexeme: "(", Literal: nil}, nil
 	case ')':
-		s.index++
+		s.advance()
 		return &token.Token{Type: token.RIGHT_PAREN, Lexeme: ")", Literal: nil}, nil
 	case '{':
-		s.index++
+		s.advance()
 		return &token.Token{Type: token.LEFT_BRACE, Lexeme: "{", Literal: nil}, nil
 	case '}':
-		s.index++
+		s.advance()
 		return &token.Token{Type: token.RIGHT_BRACE, Lexeme: "}", Literal: nil}, nil
 	case ',':
-		s.index++
+		s.advance()
 		return &token.Token{Type: token.COMMA, Lexeme: ",", Literal: nil}, nil
 	case '.':
-		s.index++
+		s.advance()
 		return &token.Token{Type: token.DOT, Lexeme: ".", Literal: nil}, nil
 	case '*':
-		s.index++
+		s.advance()
 		return &token.Token{Type: token.STAR, Lexeme: "*", Literal: nil}, nil
 	case '+':
-		s.index++
+		s.advance()
 		return &token.Token{Type: token.PLUS, Lexeme: "+", Literal: nil}, nil
 	case '-':
-		s.index++
+		s.advance()
 		return &token.Token{Type: token.MINUS, Lexeme: "-", Literal: nil}, nil
 	case ';':
-		s.index++
+		s.advance()
 		return &token.Token{Type: token.SEMICOLON, Lexeme: ";", Literal: nil}, nil
 	case '=':
-		s.index++
-		if !s.isAtEnd() && s.input[s.index] == '=' {
-			s.index++
+		s.advance()
+		if s.peak() == '=' {
+			s.advance()
 			return &token.Token{Type: token.EQUAL_EQUAL, Lexeme: "=", Literal: nil}, nil
 		}
 		return &token.Token{Type: token.EQUAL, Lexeme: "=", Literal: nil}, nil
 	case '!':
-		s.index++
-		if !s.isAtEnd() && s.input[s.index] == '=' {
-			s.index++
+		s.advance()
+		if s.peak() == '=' {
+			s.advance()
 			return &token.Token{Type: token.BANG_EQUAL, Lexeme: "!=", Literal: nil}, nil
 		}
 		return &token.Token{Type: token.BANG, Lexeme: "!", Literal: nil}, nil
 	case '<':
-		s.index++
-		if !s.isAtEnd() && s.input[s.index] == '=' {
-			s.index++
+		s.advance()
+		if s.peak() == '=' {
+			s.advance()
 			return &token.Token{Type: token.LESS_EQUAL, Lexeme: "<=", Literal: nil}, nil
 		}
 		return &token.Token{Type: token.LESS, Lexeme: "<", Literal: nil}, nil
 	case '>':
-		s.index++
-		if !s.isAtEnd() && s.input[s.index] == '=' {
-			s.index++
+		s.advance()
+		if s.peak() == '=' {
+			s.advance()
 			return &token.Token{Type: token.GREATER_EQUAL, Lexeme: ">=", Literal: nil}, nil
 		}
 		return &token.Token{Type: token.GREATER, Lexeme: ">", Literal: nil}, nil
 	case '/':
-		s.index++
-		if !s.isAtEnd() && s.input[s.index] == '/' {
-			for ; !s.isAtEnd(); s.index++ {
-				if s.input[s.index] == '\n' {
-					s.index++
+		s.advance()
+		if s.peak() == '/' {
+			for ; !s.isAtEnd(); s.advance() {
+				if s.peak() == '\n' {
+					s.advance()
 					s.line++
 					break
 				}
@@ -99,38 +99,38 @@ func (s *Scanner) Scan() (*token.Token, error) {
 		}
 		return &token.Token{Type: token.SLASH, Lexeme: "/", Literal: nil}, nil
 	case ' ', '\r', '\t':
-		s.index++
+		s.advance()
 		return nil, nil
 	case '\n':
-		s.index++
+		s.advance()
 		s.line++
 		return nil, nil
 	case '"':
-		s.index++
+		s.advance()
 		var literal string
-		for !s.isAtEnd() && s.input[s.index] != '"' {
-			literal += string(s.input[s.index])
-			s.index++
+		for s.peak() != '"' {
+			literal += string(s.peak())
+			s.advance()
 		}
 		if s.isAtEnd() {
 			var err = fmt.Errorf("[line %d] Error: Unterminated string.", s.line)
 			return nil, err
 		}
-		s.index++
+		s.advance()
 		return &token.Token{Type: token.STRING, Lexeme: fmt.Sprintf("\"%s\"", literal), Literal: literal}, nil
 	case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
 		var literal string
-		for s.isDigit(s.input[s.index]) {
-			literal += string(s.input[s.index])
-			s.index++
+		for s.isDigit(s.peak()) {
+			literal += string(s.peak())
+			s.advance()
 		}
 
-		if s.input[s.index] == '.' && s.isDigit(s.peak()) {
-			literal += string(s.input[s.index])
-			s.index++
-			for s.isDigit(s.input[s.index]) {
-				literal += string(s.input[s.index])
-				s.index++
+		if s.peak() == '.' && s.isDigit(s.peakNext()) {
+			literal += string(s.peak())
+			s.advance()
+			for s.isDigit(s.peak()) {
+				literal += string(s.peak())
+				s.advance()
 			}
 		}
 
@@ -138,21 +138,33 @@ func (s *Scanner) Scan() (*token.Token, error) {
 
 		return &token.Token{Type: token.NUMBER, Lexeme: literal, Literal: num}, nil
 	default:
-		var err = fmt.Errorf("[line %d] Error: Unexpected character: %c", s.line, s.input[s.index])
-		s.index++
+		var err = fmt.Errorf("[line %d] Error: Unexpected character: %c", s.line, s.peak())
+		s.advance()
 		return nil, err
 	}
 }
 
-func (s *Scanner) isAtEnd() bool {
-	return s.index >= len(s.input)
+func (s *Scanner) advance() byte {
+	s.index++
+	return s.input[s.index-1]
 }
 
 func (s *Scanner) peak() byte {
+	if s.isAtEnd() {
+		return 0
+	}
+	return s.input[s.index]
+}
+
+func (s *Scanner) peakNext() byte {
 	if s.index+1 >= len(s.input) {
 		return 0
 	}
 	return s.input[s.index+1]
+}
+
+func (s *Scanner) isAtEnd() bool {
+	return s.index >= len(s.input)
 }
 
 func (s *Scanner) isDigit(c byte) bool {
