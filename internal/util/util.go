95fc2ec2a3d8ca52088f52bcf03a801dd97a3f1d
pooyaht <pooyahatami83@gmail.com>
2025-05-29 08:42:13 +0330

    Fix large number formatting to avoid scientific notation

--- internal/util/util.go
+++ internal/util/util.go
@@ -6,7 +6,15 @@ import (
 )
 
 func FormatFloat(num float64) string {
-	numStr := strconv.FormatFloat(num, 'f', -1, 64)
+	defaultStr := strconv.FormatFloat(num, 'f', -1, 64)
+
+	var numStr string
+	if strings.Contains(defaultStr, "e") || strings.Contains(defaultStr, "E") {
+		numStr = strconv.FormatFloat(num, 'f', 1, 64)
+	} else {
+		numStr = defaultStr
+	}
+
 	if !strings.Contains(numStr, ".") {
 		return numStr + ".0"
 	}
@@ -14,7 +22,7 @@ func FormatFloat(num float64) string {
 	parts := strings.Split(numStr, ".")
 	parts[1] = strings.TrimRight(parts[1], "0")
 	if len(parts[1]) == 0 {
-		parts[1] = ""
+		parts[1] = "0"
 	}
 	return strings.Join(parts, ".")
 }
